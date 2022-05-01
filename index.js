79e419b8485cec7bd5dc06f189f06f60c37d3f87
sreevidya <sreevidyachowlur@gmail.com>
2022-05-01 22:31:47 +0530

    port changed

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+var port = process.env.PORT || 3000;
 
 require("./db");
 
 app.use("/User", require("./controller/user-controller"));
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`listening on port 3000,server started..`);
 });
